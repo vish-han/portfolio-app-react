470c71bddacea3b0c75aaa1229123069c63dec8c
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-08-28 13:07:01 +0530

    Test done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,14 +14,12 @@ const About = () => {
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Clint, nice to meet you. Please take a look around.</p>
+              <p>Hi. I'm Vishal Chauhan, nice to meet you. Please take a look around.</p>
             </div>
             <div>
               <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
+              the lives of those around me. I am a Student who love to make projects and learn new Stuff.
+              I love to participate in hackathons and meet new people.</p>  
             </div>
           </div>
       </div>
