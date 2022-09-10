bf47b77aff3eaa273144f7fb58c76b42c73bf5e0
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-09-10 11:44:16 +0530

    scrol bar cutomized"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,14 +20,14 @@ const Home = () => {
           building responsive full-stack web applications.
         </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
         <Link to='work' smooth={true} duration={500}> 
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
             View Work
             <span className='group-hover:rotate-90 duration-300'>
              <HiArrowNarrowRight className='ml-3 ' />
             </span>
-        </Link>
           </button>
+        </Link>
         </div>
       </div>
     </div>
