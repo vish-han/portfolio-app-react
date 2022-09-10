f784cc480da973dc674f84da9a975d1f01e9c76c
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-09-10 11:37:44 +0530

    loading added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,35 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import './index.css';
+import React from "react";
 
 
 function App() {
+  const [loading,setLoading] = React.useState(false)
+  React.useEffect(()=>{
+setLoading(true)
+setTimeout(()=>{
+setLoading(false)
+},1000)
+  },[])
   return (
+  
     <div>
+    {  loading? (
+        <div className="loader-container">
+        <div className="spinner"></div>
+        
+      </div> ) : (
+        <div>
       <Navbar />
       <Home />
       <About />
       <Skills />
       <Work />
       <Contact />
+      </div>
+)}
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import Link from 'react-scroll/modules/components/Link';
 
 const Home = () => {
   return (
@@ -20,10 +21,12 @@ const Home = () => {
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+        <Link to='work' smooth={true} duration={500}> 
             View Work
             <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3 ' />
+             <HiArrowNarrowRight className='ml-3 ' />
             </span>
+        </Link>
           </button>
         </div>
       </div>
