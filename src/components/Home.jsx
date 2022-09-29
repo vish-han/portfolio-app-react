57635b46b0499209995ace929c3e6842c502324c
Vishal Chauhan <vshlchauhab@gmail.com>
2022-09-29 20:58:04 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           Vishal Chauhan
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a Front End Developer
+          I'm a Full Stack Developer
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
           I’m a full-stack developer specializing in building (and occasionally
